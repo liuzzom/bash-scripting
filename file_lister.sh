b9b36bb96df32e147e6e4ea3e5ac5771f56934e0
liuzzom <33182647+liuzzom@users.noreply.github.com>
2018-04-30 13:02:43 +0200

    Add files via upload

--- file_lister.sh
+++ file_lister.sh
@@ -2,8 +2,8 @@
 # verifica se è stata inserita l'opzione -n
 if [ "$1" == "-n" ]; then
 	#verifica se è stato inserito un path
-	if [ -n $1 ]; then
-		ls -l $1 | grep '^-' | awk '$5 != 0 {printf "file: %s size: %d\n", $NF, $5}'
+	if [ -n $2 ]; then
+		ls -l $2 | grep '^-' | awk '$5 != 0 {printf "file: %s size: %d\n", $NF, $5}'
 	else
 		ls -l | grep '^-' | awk '$5 != 0 {printf "file: %s size: %d\n", $NF, $5}'
 	fi	
