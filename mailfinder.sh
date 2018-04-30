81e5fb7874948bbfffd9c05b9585ec7fb6216590
liuzzom <33182647+liuzzom@users.noreply.github.com>
2018-04-30 17:01:59 +0200

    Add files via upload

--- mailfinder.sh
+++ mailfinder.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-awk '/[a-z]+\.[a-z]+@[a-z]+\.[a-z]+/' $1/[a-z]*[0-9].txt
+if [ -n $1 ]
+then
+	awk '/[a-z]+\.[a-z]+@[a-z]+\.[a-z]+/' $1/[a-z]*[0-9].txt
+else
+	awk '/[a-z]+\.[a-z]+@[a-z]+\.[a-z]+/' ./[a-z]*[0-9].txt
+fi
